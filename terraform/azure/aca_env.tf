52303ea267061f676fb080e3d790046b25128371
Kumar Saraboji <er.ksaraboji@example.com>
2026-02-09 15:53:21 -0500

    Add Azure Container Apps infrastructure (Gateway, Ollama, RAG, Speech)

---
+++ terraform/azure/aca_env.tf
@@ -0,0 +1,14 @@
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "databro-${var.environment}-logs"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "main" {
+  name                       = "databro-${var.environment}-env"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+}
